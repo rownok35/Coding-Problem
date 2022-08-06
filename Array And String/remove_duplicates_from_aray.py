bc39c6b1c7883cd3d1ef05cf00b986985efab528
rownok35 <rownokrezwan@gmail.com>
2022-08-06 13:39:04 +0600

    adding problems in array and strings

---
+++ Array And String/remove_duplicates_from_aray.py
@@ -0,0 +1,16 @@
+def remove_duplicates(nums):
+    n = len(nums)
+
+    current_index = 1
+    for i in range(1, n):
+        if nums[i] != nums[i-1]:
+            nums[current_index] = nums[i]
+            current_index += 1
+    
+    return current_index
+
+
+if __name__ == '__main__':
+    nums = [1, 1, 2, 2, 3, 4, 5, 5, 6, 6, 7, 7]
+    l = remove_duplicates(nums)
+    print(nums[:l])
